0239624cb1739716dc319f17b1dbda837011e0dc
Nneif <nneifemakpunonu@gmail.com>
2023-08-14 10:11:52 -0400

    added some hover and more style

--- portfolio/src/pages/About.jsx
+++ portfolio/src/pages/About.jsx
@@ -7,7 +7,7 @@ export default function About(){
         <div className="about-me">
         <h3>About me</h3>
         <img src={selfieImage} className="selfie" alt="picture of nneif"></img>
-        <p className="bio">Hello, I'm Nneif Akpunonu, currently as information techology student at Montgomery County Community College. Did not get into coding until mt first year of college in my first coding class when we briefly talked about HTML and CSS. It was the first time I enjoyed coding and deciding to purse a career as a Front-End Developer.</p>
+        <p className="bio">Hello, I'm Nneif Akpunonu, currently as information techology student at Montgomery County Community College. Did not get into coding until my first year of college in my first coding class when we briefly talked about HTML and CSS. It was the first time I enjoyed coding and deciding to purse a career as a Front-End Developer.</p>
         </div>
         </>
     )
--- portfolio/src/pages/Project.jsx
+++ portfolio/src/pages/Project.jsx
@@ -13,36 +13,42 @@ export default function Project(){
             name: "Recipedia",
             githubLink: "https://github.com/ruggerheist/recipedia",
             websiteLink: "https://ruggerheist.github.io/recipedia/",
+            description: 'discover some new recipes',
             image: recipediaImage,
         },
         {
             name: "Bookbusta",
             githubLink: "https://github.com/Nneifem/bookbusta",
             websiteLink: "https://bookbusta-950538aa625f.herokuapp.com/",
+            description: 'create an account, find books, and leave reviews',
             image: bookbustaImage,
         },
         {
             name: "Weather Dashboard",
             githubLink: "https://github.com/Nneifem/weather-dashboard",
             websiteLink: "https://nneifem.github.io/weather-dashboard/",
+            description: 'checkout the weather anywhere for the next 5 days',
             image: weatherDashboardImage,
         },
         {
             name: "Text Editor",
             githubLink: "https://github.com/Nneifem/text-editor",
             websiteLink: "https://nneif-text-editor-2d4fcad3ac4f.herokuapp.com/",
+            description: 'write some text online or offline',
             image: textEditorImage,
         },
         {
             name: "Note Taker",
             githubLink: "https://github.com/Nneifem/note-taker",
             websiteLink: "https://nneif-note-taker.herokuapp.com/",
+            description: 'create, update, and delete notes',
             image: noteTakerImage,
         },
         {
             name: "Password Generator",
             githubLink: "https://github.com/Nneifem/password-generator",
             websiteLink: "https://nneifem.github.io/password-generator/",
+            description: 'generate a new password',
             image: passwordGeneratorImage,
         },
 
@@ -51,23 +57,20 @@ export default function Project(){
     return(
         <section>
             <h3 className="project-name">My Projects</h3>
-            <ul>
+            <ul className="projects">
                 {
                     projects.map((project, i) => (
                         <li key = {i}>
-                            <div className="container text-center">
-                            <div className="row row-cols-2">
-                                <div className="col">
-                                    <div className="card">
-                                <img src={project.image} className="img-fluid rounded-start" alt="each picture image"></img>
-                                    <div className="card-title">
-                                        <a href={project.websiteLink}>{project.name}</a>
-                                    </div>
-                                        <a href={project.githubLink}><img src={githubLogo} className="github-image" alt="github logo"></img></a>
-                                    </div>
-                                </div>
+                        <div className="project-section">
+                            <img src={project.image} className="image" alt="each picture image"></img>
+                            <div className="project-info">
+                                <div className="project-title">
+                                    <a href={project.websiteLink} style={{color: "38220f"}}>{project.name}</a>
                             </div>
+                            <a href={project.githubLink}><img src={githubLogo} className="github-image" alt="github logo"></img></a>
+                            <p>{project.description}</p>
                             </div>
+                        </div>
                         </li>
                     ))
                 }
