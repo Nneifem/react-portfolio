9b24f3212c053bc14c6e30a3e1c062bcd9709868
Nneif <nneifemakpunonu@gmail.com>
2024-05-30 19:36:22 -0400

    changning the links for the UI page

--- src/pages/UI.jsx
+++ src/pages/UI.jsx
@@ -7,15 +7,15 @@ export default function UI(){
         {
             name: 'Walmart',
             task: 'take a "bad" website and redesign it',
-            desktopLink: 'https://lnkd.in/eG8AdMCd',
-            mobileLink: 'https://lnkd.in/eeCsJ9Dc',
+            desktopLink: 'https://www.figma.com/proto/ErFebmZKeM15ViVj4poYct/Akpunonu-Desktop-Home%2FService%2FDepartment?node-id=1-2&t=rqIi2ZAjimBRfcVS-1&scaling=min-zoom&page-id=0%3A1&starting-point-node-id=1%3A2',
+            mobileLink: 'https://www.figma.com/proto/yIlTMwFlKDZ2JMlDJUZZhX/Akpunonu-Mobile-Home%2FService%2FDepartment?t=qUpfdMmYU7b6uXyM-1&scaling=scale-down&page-id=0%3A1&node-id=1-2&starting-point-node-id=1%3A2',
             image: walmartImage
         },
         {
             name: 'P O V',
             task: 'poetry webiste designed for poetry lovers',
-            desktopLink: 'https://lnkd.in/eB3G-y-X',
-            mobileLink: 'https://lnkd.in/eMEHm57i',
+            desktopLink: 'https://www.figma.com/proto/gJr5T2tzTzjaL2U0jbliD8/Akpunonu-POV-High-Fidelity?node-id=3-2&t=wXCEtHgWoEBty7xH-1&scaling=scale-down&page-id=0%3A1&starting-point-node-id=3%3A2',
+            mobileLink: 'https://www.figma.com/proto/cePZjpFxpY5M613AqdyAnk/Akpunonu-POV-High-Fidelity?node-id=2-2&t=xLFuhzRg2x4VSnJs-1&scaling=scale-down&page-id=0%3A1&starting-point-node-id=3%3A4',
             image: povImage
         },
     ]
