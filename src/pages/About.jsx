33b73365bde97bc0a67f97aee38942c212b4ec0b
Nneif <nneifemakpunonu@gmail.com>
2026-03-30 19:42:47 -0400

    updated about me page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,7 +4,7 @@ export default function About(){
     return(
         <>
         <div className="about-me">
-        <p className="bio"> <h2 className="opener">Hello,</h2> I'm Nneif Akpunonu and I'm a IT student and a full stack developer. <br /> <br />I didn't get into coding until my freshman year of college and that's when I discovered my love for web development, specifically the front-end developing. Earning my certificate at University of Pennsylvania, I fell in love with coding and getting the excitement of when I figured out a solution to a problem and seeing the completed websites I built. I want to be able to continue this journey and be able to make website for others and make their vision come to life. <br /> <br />For any inquiries or questions, please reach me by email at: <a href="mailto:nneifemakpunonu@gmail.com">nneifemakpunonu@gmail.com</a></p>
+        <p className="bio"> <h2 className="opener">Hello,</h2> I'm Nneif Akpunonu and I'm a IT student with a focus on Application Development. <br /> <br />I didn't get into coding until my freshman year of college and that's when I discovered my love for web development, specifically the front-end developing. Earning my certificate for Full Stack Development at University of Pennsylvania, I fell in love with coding and getting the excitement of when I figured out a solution to a problem and seeing the completed websites I built. I want to be able to continue this journey and be able to make website for others and make their vision come to life. <br /> <br />For any inquiries or questions, please reach me by email at: <a href="mailto:nneifemakpunonu@gmail.com">nneifemakpunonu@gmail.com</a></p>
         </div>
         </>
     )
